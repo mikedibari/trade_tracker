ec969c46fba43ee98bd1e5aa1be9a3c7bfd85617
Mike DiBari <mike.dibari@comcast.net>
2023-05-14 15:31:47 -0400

    use dummy sqlite db for testing

--- stocks/settings_github.py
+++ stocks/settings_github.py
@@ -3,6 +3,11 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    },
 }
+
