830acdb9c5177693b87b8ce4a393fcd0deb39752
Mike DiBari <mike.dibari@comcast.net>
2023-05-14 22:11:31 -0400

    test using settings_github.py

--- stocks/settings_github.py
+++ stocks/settings_github.py
@@ -1,5 +1,6 @@
 from .settings import *
 
+# Use a different database for testing
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -8,6 +9,33 @@ DATABASES = {
     'test': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'test_db.sqlite3',
-    },
+    }
 }
 
+# Set the test runner
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Use in-memory caching for tests
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
+# Disable logging during tests
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+    },
+}
